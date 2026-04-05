378891bfeef33736ba5afd6d5fe01e65440090ca
Amr Elganainy <amrmohammedali11@gmail.com>
2026-04-05 02:36:14 +0200

    Merge run_ui_fixed.py logic into run_ui.py, delete run_ui_fixed.py, leave .planning/ out

--- run_ui.py
+++ run_ui.py
@@ -1,5 +1,14 @@
-#!/usr/bin/env python
 """Entry point for mobile-crawler GUI application."""
+
+#!/usr/bin/env python
+"""Entry point for mobile-crawler GUI application with fixed path."""
+
+import sys
+import os
+
+# Add src directory to path
+src_path = os.path.join(os.path.dirname(__file__), 'src')
+sys.path.insert(0, src_path)
 
 from mobile_crawler.ui.main_window import run
 
